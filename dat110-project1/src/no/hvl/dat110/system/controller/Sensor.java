0e2f9d62446b941ace5c5d598f972e83d789df5f
Kjetil Stokke <54102543+h131177@users.noreply.github.com>
2020-02-28 11:30:58 +0100

    Update Sensor.java

--- dat110-project1/src/no/hvl/dat110/system/controller/Sensor.java
+++ dat110-project1/src/no/hvl/dat110/system/controller/Sensor.java
@@ -8,13 +8,20 @@ public class Sensor extends RPCStub {
 	private byte RPCID = 1;
 	
 	public int read() {
-		rpcclient.connect();
-		byte[] byteSend = RPCUtils.marshallInteger(RPCID, 0);
-		byte[] byteRec = rpcclient.call(byteSend);
-		int temp = RPCUtils.unmarshallInteger(byteRec);
+		//rpcclient.connect();
+		//byte[] byteSend = RPCUtils.marshallInteger(RPCID, 0);
+		//byte[] byteRec = rpcclient.call(byteSend);
+		//int temp = RPCUtils.unmarshallInteger(byteRec);
+		int temp;
 		
 		// TODO
 		// implement marshalling, call and unmarshalling for read RPC method
+		
+		byte[] request = RPCUtils.marshallVoid(RPCID);
+		
+		byte[] response = rpcclient.call(request);
+		
+		temp = RPCUtils.unmarshallInteger(response);
 		
 		/*if (true) {
 			throw new UnsupportedOperationException(TODO.method());
